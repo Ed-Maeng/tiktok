d81148f6ae8723cad82ad2dfb4cbd02014049ed1
Edward Maeng <emaeng@vmware.com>
2023-01-17 05:50:38 +0000

    Set up for Sanity, Next, TypeScript, and Tailwind CSS

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      TikTok
+    </div>
+  )
+}
+
+export default Home
---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ocq65959',
+    dataset: 'production'
+  }
+})
